79c61702e207ca265a12942cb84ba1d68d15fe94
Nishanth T <nishantht27@gmail.com>
2026-05-28 19:57:36 +0530

    Enhance stone paper scissor game output
    Updated game messages and improved user feedback.

--- stone paper scissor game.py
+++ stone paper scissor game.py
@@ -1,10 +1,15 @@
 import random
-a=input('stone...paper...scissor...?')
-print('\n',a)
-item=('stone','paper','scissor')
-b= random.choice(item)
-print(b)
-if (a==b):
-    print('YOU WON!!')
+
+a = input('stone...paper...scissor...? ')
+print('\nYou chose:', a)
+item = ('stone', 'paper', 'scissor')
+b = random.choice(item)
+print('Computer chose:', b)
+if a == b:
+    print('DRAW!!')
+elif (a == 'stone' and b == 'scissor') or \
+     (a == 'paper' and b == 'stone') or \
+     (a == 'scissor' and b == 'paper'):
+    print('YOU WON!! 🔥')
 else:
-    print('BETTER LUCK NXT TIME')
+    print('BETTER LUCK NEXT TIME 😅')
