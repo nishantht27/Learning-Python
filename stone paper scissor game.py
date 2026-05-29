98438d970ea5cb2291aa08d7fae74296f23703e8
NishanthT <Nishantht27@gmail.com>
2026-05-29 13:24:43 +0530

    Added local Python programs

---
+++ stone paper scissor game.py
@@ -0,0 +1,17 @@
+import random
+
+while True :
+  a = input('stone...paper...scissor...? ').lower()
+  print('\nYou chose:', a)
+  item = ('stone', 'paper', 'scissor')
+  b = random.choice(item)
+  print('Computer chose:', b)
+  if a == b:
+       print('DRAW!!')
+  elif (a == 'stone' and b == 'scissor') or \
+       (a == 'paper' and b == 'stone') or \
+       (a == 'scissor' and b == 'paper'):
+       print('YOU WON!! 🔥')
+       break
+  else:
+       print('BETTER LUCK NEXT TIME 😅')
