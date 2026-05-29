3f5918de3d8ca901431d22912a624c6acd080e12
Nishanth T <nishantht27@gmail.com>
2026-05-29 07:10:56 +0530

    Update stone paper scissor game.py

--- stone paper scissor game.py
+++ stone paper scissor game.py
@@ -1,15 +1,17 @@
 import random
 
-a = input('stone...paper...scissor...? ')
-print('\nYou chose:', a)
-item = ('stone', 'paper', 'scissor')
-b = random.choice(item)
-print('Computer chose:', b)
-if a == b:
-    print('DRAW!!')
-elif (a == 'stone' and b == 'scissor') or \
-     (a == 'paper' and b == 'stone') or \
-     (a == 'scissor' and b == 'paper'):
-    print('YOU WON!! 🔥')
-else:
-    print('BETTER LUCK NEXT TIME 😅')
+while True :
+  a = input('stone...paper...scissor...? ')
+  print('\nYou chose:', a)
+  item = ('stone', 'paper', 'scissor')
+  b = random.choice(item)
+  print('Computer chose:', b)
+  if a == b:
+      print('DRAW!!')
+  elif (a == 'stone' and b == 'scissor') or \
+       (a == 'paper' and b == 'stone') or \
+       (a == 'scissor' and b == 'paper'):
+      print('YOU WON!! 🔥')
+      break
+  else:
+      print('BETTER LUCK NEXT TIME 😅')
